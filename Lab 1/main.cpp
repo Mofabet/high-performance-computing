0aeaed1b90789c6e9a58d7abe1d16420ef25a20c
Mofabet <gutamaya@outlook.com>
2020-10-20 21:29:53 +0500

    send 2

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -12,17 +12,21 @@ using namespace std;
 
 int main () {
     double F(double u,double t, double r);
-    double x, t, f, bigD, bigC;
+    double x, t, f, bigD, bigC, bigX;
     double dx, dt;
     double K, K1, K2;
-    int A, B, c;
+    int A, B, C, N;
 
     //U[(int)n]; double T[(int)n]; double R[(int)n]; // det n as a constant
     //U[0]=a; //T-R
 
+    //определение переменных (вынести отдельно?)
+    // cin - ввод с клав-ры
+
 //расчет С
 
 
+    //
     bigC = 2*bigD*(dt/dx/dx);
     cout << " The ratio of the densities of meshes: C = " << bigC << endl;
 
@@ -31,16 +35,18 @@ int main () {
     }
 
     
-    for(int i) i=1; i<=n; i++){
-         X[i]=a+i*h;
-         Y[i]=Y[i-1]+h*F(X[i-1],Y[i-1]);
+    for(int i) i = 1; i <= N-1; i++){
+         bigX[i] = 0;
+         //Y[i]=Y[i-1]+h*F(X[i-1],Y[i-1]);
     }
-    for(int i=0; i<=n; i++){
+
+    for(int i=0; i <= N; i++){
+
         cout << "U["<<i<<"]="<<X[i] <<" ";
     }
     cout << endl;
     for(int i=0; i<=n; i++){
-        cout << "T["<<i<<"]="<<Y[i] << " ";
+        cout << "T["<<i<<"]="<<Y[i] << " "; //re all
     }
     return 0;
 }
