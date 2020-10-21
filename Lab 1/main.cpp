70cc2679e18e409ab009ca94fa88a97637ce6054
Mofabet <gutamaya@outlook.com>
2020-10-21 14:43:12 +0500

    send 2+

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -12,11 +12,12 @@ using namespace std;
 
 int main () {
     double F(double u,double t, double r);
-    double x, t, f, bigD, bigC, bigX;
+    double x, t, f;
+    double U, C, D;
     double dx, dt;
     double K1, K2;
-    int A, B, C, Ns;
-    int i, k;
+    int i, k, Ns;
+    int A, B; // counters
 
     //U[(int)n]; double T[(int)n]; double R[(int)n]; // det n as a constant
     //U[0]=a; //T-R
@@ -28,26 +29,26 @@ int main () {
 
 
     //
-    bigC = 2*bigD*(dt/dx/dx);
-    cout << " The ratio of the densities of meshes: C = " << bigC << endl;
+    C = 2*D*(dt/dx/dx);
+    cout << " The ratio of the densities of meshes: C = " << C << endl;
 
-    if (bigC > 1) {
+    if (C > 1) {
         //проверить устойчивость
     }
 
     
     for(int i) i = 1; i <= Ns-1; i++){
-         bigX[i] = 0;
+         U[i] = 0;
          //Y[i]=Y[i-1]+h*F(X[i-1],Y[i-1]);
     }
     // задание граничных условий 
     // Кельв1 - Tl, Кельв 2 - Tr
 
     for (int k = 0; k <= A; k++){
-        bigX[0][k] = K1; //??? 
-        bigX[Ns-1][k] = K2; // теперь обане работают
+        U[0][k] = K1; //??? 
+        U[Ns-1][k] = K2; // теперь обане работают
         for (int i ...){
-        //U[i]=(bigC/2)*U[i-1][k]+(1-bigC)*U[i][k]+(bigC/2)*U[i+1][k]+(dtf);
+        //U[i]=(C/2)*U[i-1][k]+(1-C)*U[i][k]+(C/2)*U[i+1][k]+(dtf);
         }
     }
 
